1205eb458261f24eaf0e2cec075f07bc14ec7ede
BartOuwehand <b.k.ouwehand@gmail.com>
2021-03-01 23:38:02 +0100

    Seperated the 'meta-data' & flux values in different files

--- script2.py
+++ script2.py
@@ -5,6 +5,7 @@ from astropy import wcs
 from astropy.io import ascii
 from astropy.table import Table, Column, MaskedColumn
 from pathlib import Path
+from math import trunc
 
 # Import coordinates from selected objects
 standards = ascii.read("standards.txt", guess=False)
@@ -22,21 +23,22 @@ def find_stars(xobj_list, yobj_list, xlist, ylist, f):
     distance_matrix = np.sqrt((xmatrix-xobj_list)**2 + (ymatrix-yobj_list)**2)
     
     indices = np.argmin(distance_matrix, axis=0)
-    """
+    
     # Make plot to check data
     plt.figure(figsize=(16,16))
-    plt.scatter(xlist,ylist,c='b',)
-    plt.scatter(xobj_list, yobj_list, c='red',marker='.')
+    plt.scatter(xlist,ylist,c='b',s=4)
+    plt.scatter(xobj_list, yobj_list, c='red', s=2)
+    plt.plot([2000,2025],[0,0],c='r')
     for i in range(nr_stars):
-    	plt.plot([xobj_list[i],xlist[indices[i]]],[yobj_list[i],ylist[indices[i]]], linestyle='dashed',c='r')
+    	plt.plot([xobj_list[i],xlist[indices[i]]],[yobj_list[i],ylist[indices[i]]], linestyle='dashed',c='r',linewidth=1)
     plt.title(f)
     plt.show()
-    """
+    
     return indices, np.diag(distance_matrix[indices])
     #the diagonal gives the distance from each selected object to the closest object
 
 # Make one file with the image_name, epoch, filter and airmass
-meta_data = [] #where each row is the meta-data from one file
+t_image, t_newimg, t_epoch, t_filter, t_airmass = [], [], [], [], []
 
 #selects all names in ../data/
 for filename in Path('..//data/').rglob('*e91.fits.fz'):
@@ -46,56 +48,55 @@ for filename in Path('..//data/').rglob('*e91.fits.fz'):
 	hdu1 = fits.open(filename)
 	imh = hdu1['SCI'].header
 	phot = hdu1['cat'].data
-
+	
+	# Getting the meta-data from the .fits file and writing it to lists
 	epoch = imh['MJD-OBS']
 	obs_filter = imh['FILTER']
-	airmass = imh['AIRMASS']
-	obj = filename.replace('../data/','')
-	meta_data.append([obj, epoch, obs_filter,airmass])
+	t_image.append(filename.replace('../data/',''))
+	t_epoch.append(epoch)
+	t_filter.append(obs_filter)
+	t_airmass.append(imh['AIRMASS'])
 	
 	# Transform the world coordinates for the selected stars into pixel coordinates
 	w = wcs.WCS(hdu1['SCI'].header)
 	coord = w.wcs_world2pix(coord_world,0)
 	
+	# Make a new file path to write to with name= str( [EPOCH to 5 decimals (~1s)]_[FILTER])
+	newfilename = '../datared/'+str(round(trunc(epoch*1e5)*1e-5,6)) + '_' + obs_filter + '.txt'
+	t_newimg.append(newfilename)
 	# Use the find_stars function to get the closest-star-indices for the selected stars
-	stars_arg, d = find_stars(coord[:,0],coord[:,1], phot['x'], phot['y'], filename)
-	print (d)
+	stars_arg, d = find_stars(coord[:,0],coord[:,1], phot['x'], phot['y'], newfilename)
 	
 	# Use the indices to get the flux & fluxerr for all selected stars
 	flux_stars = phot['flux'][stars_arg]
 	fluxerr_stars = phot['fluxerr'][stars_arg]
 	#if the distance is more than 25 pixels (~9.7"), it's too far away so it will write out NaN
 	select_mask = d > 25
-	flux_stars[d] = np.NaN
-	fluxerr_stars[d] = np.NaN
+	flux_stars[select_mask] = np.NaN
+	fluxerr_stars[select_mask] = np.NaN
+	
+	if np.sum(select_mask) > .25*nr_stars:
+		print ("Bad data?" + filename)
 	
 	
-	# Make a new file path to write to
-	newfilename = 
-	
-	#VERY cheap fix for "ValueError: Inconsistent data column lengths: {1, 44}", must be a better way
-	# maybe use 2 different files, maybe
-	nanlist = [np.NaN]*(nr_stars-1)
-
-	obj = [filename.replace('../data/','').replace('.fits.fz','')]
-	obj.extend(nanlist)
-	epoch = [imh['MJD-OBS']]
-	epoch.extend(nanlist)
-	obs_filter = [imh['FILTER']]
-	obs_filter.extend(nanlist)
-	airmass = [imh['AIRMASS']]
-	airmass.extend(nanlist)
-
 
 	# Write data to Table and then to file
-	filecontent = Table([obj, epoch, obs_filter, airmass, flux_stars, fluxerr_stars], \
-				names=['IMAGE','EPOCH','FILTER','AIRMASS','FLUX','FLUXERR'])
+	filecontent = Table([range(1,nr_stars+1),flux_stars, fluxerr_stars], \
+				names=['STARNR','FLUX','FLUXERR'])
 	ascii.write(filecontent, newfilename, overwrite=True)
 
 # Write out the meta_data elements to a file
-filecontent = Table([meta_data[0,:],meta_data[1,:],meta_data[2,:],meta_data[3,:]], \
-				names=['IMAGE','EPOCH','FILTER','AIRMASS'])
+filecontent = Table([t_image,t_newimg, t_epoch, t_filter, t_airmass], \
+				names=['IMAGE','NEWIMG','EPOCH','FILTER','AIRMASS'])
 filepath = "..//datared/J0600_meta-data.txt"
 ascii.write(filecontent, filepath, overwrite=True)
+
+
+
+
+
+
+
+
 
 
